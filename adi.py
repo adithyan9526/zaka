de1530342de5236b6422555c408f6ccab42534de
adithyan9526 <adithyannp80@gmail.com>
2024-10-04 16:06:32 +0530

    Add files via upload

---
+++ adi.py
@@ -0,0 +1,53 @@
+print("hello,world!")
+print("hello\nworld")
+if 5>4:
+ print("five is greater than four!")
+
+if 7>2:
+ print("seven is greater than two")
+ print("four is greater than 1")
+
+x=5
+x="joseph"
+print(x)
+
+
+x=str(3)
+y=int(3)
+z=float(3)
+print(x)
+print(y)
+print(z)
+
+x=5
+y="joseph"
+z=6.5
+u=6j
+print(type(x))
+print (type(y))
+print(type(z))
+print(type(u))
+a=6
+A="JOSEPH"
+
+x , y ,z = "orange", "yellow", "red"
+print(x)
+print(y)
+print(z)
+
+x=" i"
+y=" dont"
+z=" know"
+print( x + y + z )
+print("username is:adi ")
+
+a= " hello "
+b = "world "
+c = a + b
+print(c)
+
+a="hello world!"
+print(a[6])
+
+b="hello world"
+print(a[2:5])
