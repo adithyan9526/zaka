fb157763467b38778d3560c3819e6d8a78712de8
Adi <adithyannp80@gmail.com>
2025-07-23 10:49:59 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,20 @@
+package dayofweek;
+
+import java.util.Scanner;
+
+public class odd {
+	public static void main(String[]args){
+	int num,i;
+    for(i=0;i<50;i++) {
+    	if(i%2==0) {
+    		continue;
+    	}
+    	else {
+    	System.out.println(i);
+    
+    	}
+    	}
+    }
+	
+    
+}
